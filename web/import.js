5ea4ee95a9c25ed8e1bfd717365c220f80885e68
Korie Drake Chaney <92071726+KorieDrakeChaney@users.noreply.github.com>
2022-01-16 11:01:35 -0600

    Update import.js

--- web/import.js
+++ web/import.js
@@ -1,13 +1,5 @@
-import * as wasm from "wasm-pack";
-
-
-wasm.greet('World')
-
-// or
-
-// const importedWasm = import('wasm-pack')
-// .then(wasm => {
-//     wasm.greet("World");
-// })
+const importedWasm = import('wasm-pack')
+.then(wasm => {
+})
 
 
